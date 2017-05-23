e61577ccac42eb9d00008cca03b92bfe0a8cfbe0
Mischa Hildebrand <mischa.hildebrand@intive-kupferwerk.com>
2017-05-23 15:35:24 +0200

    Initial Commit

---
+++ ExpPresentationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpPresentationController
+//
+//  Created by Mischa Hildebrand on 23.05.17.
+//  Copyright © 2017 Mischa Hildebrand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
